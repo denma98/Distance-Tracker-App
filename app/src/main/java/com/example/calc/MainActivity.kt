4e4c18675ed3ba6c3610b51d77e5c425eb34b680
denma98 <deepak21319@iiitd.ac.in>
2024-02-17 19:04:28 +0530

    till list and all. need distance and miles/km

--- app/src/main/java/com/example/calc/MainActivity.kt
+++ app/src/main/java/com/example/calc/MainActivity.kt
@@ -10,24 +10,30 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 
 data class Stop(val name: String, val distance: Int)
@@ -38,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mySwitch = findViewById<Switch>(R.id.switch2)
+        val mySwitch = findViewById<Switch>(R.id.switch3)
         val composeView = findViewById<ComposeView>(R.id.compose_view)
 
         val nextButton = findViewById<Button>(R.id.button10)
@@ -54,21 +60,26 @@ class MainActivity : AppCompatActivity() {
 @Composable
 fun MyApp(isChecked: Boolean, nextButton: Button) {
     // This state holds the index of the currently selected item
-    val selectedItemIndex = remember { mutableStateOf(0) }
+    val selectedItemIndex = remember { mutableIntStateOf(0) }
+
+    val density = LocalDensity.current
 
     MaterialTheme {
+//        NormalList(selectedItemIndex)
         if (isChecked) {
             LazyList(selectedItemIndex)
         } else {
-            NormalList()
+            NormalList(selectedItemIndex)
         }
     }
 
     // When the button is pressed, increment selectedItemIndex
     nextButton.setOnClickListener {
-        selectedItemIndex.value = (selectedItemIndex.value + 1) % 10
+        selectedItemIndex.intValue = (selectedItemIndex.intValue + 1) % 10
+
     }
 }
+
 
 @Composable
 fun LazyList(selectedItemIndex: MutableState<Int>) {
@@ -85,13 +96,15 @@ fun LazyList(selectedItemIndex: MutableState<Int>) {
         Stop("Stop 10", 100)
     )
 
+    val listState = rememberLazyListState()
+
     BoxWithConstraints {
         val screenHeight = constraints.maxHeight
         val halfScreenHeight = 350.dp
         Column {
             Spacer(modifier = Modifier.height(halfScreenHeight + 100.dp)) // Add 70.dp to the halfScreenHeight
 
-            LazyColumn(Modifier.height(halfScreenHeight)) { // This will take up the other half of the screen
+            LazyColumn(state = listState, modifier = Modifier.height(halfScreenHeight)) { // This will take up the other half of the screen
                 itemsIndexed(stops) { index, stop ->
                     // If this item is the selected item, highlight it
                     val color = if (index == selectedItemIndex.value) Color.LightGray else Color.White
@@ -111,12 +124,16 @@ fun LazyList(selectedItemIndex: MutableState<Int>) {
                     }
                 }
             }
+
+            LaunchedEffect(selectedItemIndex.value) {
+                listState.animateScrollToItem(selectedItemIndex.value)
+            }
         }
     }
 }
 
 @Composable
-fun NormalList() {
+fun NormalList(selectedItemIndex: MutableState<Int>) {
     val stops = listOf(
         Stop("stop 1", 10),
         Stop("Stop 2", 20),
@@ -129,36 +146,38 @@ fun NormalList() {
         Stop("Stop 9", 90),
         Stop("Stop 10", 100)
     )
+
+    val scrollState = rememberScrollState()
+
     BoxWithConstraints {
-
-        val screenHeight = constraints.maxHeight
+        val screenHeight = constraints.maxHeight/2
         val halfScreenHeight = 350.dp
-        val scrollState = rememberScrollState()
-        Column {
-            Spacer(modifier = Modifier.height(halfScreenHeight + 100.dp)) // Add 70.dp to the halfScreenHeight
 
-            Column(
-                modifier = Modifier
-//                    .height(halfScreenHeight + 100.dp) // Add 70.dp to the halfScreenHeight
-                    .verticalScroll(scrollState)
-            ) { // This will take up the other half of the screen
-                stops.forEach { stop ->
-                    ElevatedCard(
-                        elevation = CardDefaults.cardElevation(
-                            defaultElevation = 6.dp
-                        ),
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .fillMaxWidth()
-                    ) {
-                        Text(
-                            text = "${stop.name}: ${stop.distance} km",
-                            modifier = Modifier.padding(16.dp)
-                        )
-                    }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.height(halfScreenHeight + 100.dp))
+            stops.forEachIndexed { index, stop ->
+                // If this item is the selected item, highlight it
+                val color = if (index == selectedItemIndex.value) Color.LightGray else Color.White
+                ElevatedCard(
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    ),
+                    modifier = Modifier
+                        .background(color)
+                        .padding(8.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "${stop.name}: ${stop.distance} km",
+                        modifier = Modifier.padding(16.dp)
+                    )
                 }
             }
-//            Spacer(modifier = Modifier.height(70.dp)) // Add a Spacer at the end to leave space for a button
         }
     }
 }
+
